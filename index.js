703c9f08afe823a7fa913ffa8029ac908021fd30
nakorndevop <138006821+nakorndevop@users.noreply.github.com>
2023-07-31 00:00:59 +0700

    2 files modified

---
+++ index.js
@@ -0,0 +1,21 @@
+     // Import the functions you need from the SDKs you need
+     import { initializeApp } from "https://www.gstatic.com/firebasejs/10.1.0/firebase-app.js";
+     import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.1.0/firebase-analytics.js";
+     // TODO: Add SDKs for Firebase products that you want to use
+     // https://firebase.google.com/docs/web/setup#available-libraries
+
+     // Your web app's Firebase configuration
+     // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+     const firebaseConfig = {
+       apiKey: "AIzaSyBK5qDYCIwcB3ox7V0aTebPIdRZAWlNTj4",
+       authDomain: "realtimedb-a17ed.firebaseapp.com",
+       projectId: "realtimedb-a17ed",
+       storageBucket: "realtimedb-a17ed.appspot.com",
+       messagingSenderId: "860885717482",
+       appId: "1:860885717482:web:1665503e268812b8ad6293",
+       measurementId: "G-944W4BVR6H"
+     };
+
+     // Initialize Firebase
+     const app = initializeApp(firebaseConfig);
+     const analytics = getAnalytics(app);
