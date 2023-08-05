fee44f84101ebd665b648040926d2d8eef59dc17
nakorndevop <138006821+nakorndevop@users.noreply.github.com>
2023-08-05 15:50:01 +0700

    3 files modified

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+// Import stylesheets
+import './style.css';
+
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
